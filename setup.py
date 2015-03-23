78626f653f76233c7e7f5b989225fa75d12d1da6
Matteo Giordano <ilmalteo@gmail.com>
2015-03-24 00:00:26 +0100

    Prepare package

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@ from setuptools import setup
 setup(
     name='ploodood',
     version='0.1',
+    description='Generates words that "sound like real words"',
+    author = 'Matteo Giordano',
+    author_email = 'ilmalteo@gmail.com',
+    url = 'https://github.com/malteo/ploodood',
+    download_url = 'https://github.com/malteo/ploodood/tarball/0.1',
+    keywords = ['password', 'generator', 'cli', 'command line'],
+    packages=['ploodood'],
     py_modules=['ploodood'],
+    classifiers = [],
     install_requires=[
         'Click',
         'Requests',
